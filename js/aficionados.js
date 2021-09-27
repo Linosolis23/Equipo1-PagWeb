1b29612ad1ddaaf1003e9787f03c43f2c5e224bf
Linosolis23 <80449531+Linosolis23@users.noreply.github.com>
2021-09-27 10:24:23 +0200

    Add files via upload

---
+++ js/aficionados.js
@@ -0,0 +1,4 @@
+var nombre = prompt("Introduzca su nombre como aficionado "); //pedimos el nombre del aficionado
+var años =prompt("Introduzca los años que lleva como aficionado");//años que lleva el aficionado
+var equipo= "Equipo1"; //Nombre del equipo
+document.write("Hola <b>"+nombre + "</b> bienvenido a la web del equipo <b>" + equipo +"</b>.Gracias por acompañarnos durante estos <b>"+años+"</b> años");//mensaje una vez introduzcas los datos
