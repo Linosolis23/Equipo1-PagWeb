abd629965a2d434487cc10722db99b457f9849ff
adrik1402 <74346933+adrik1402@users.noreply.github.com>
2021-09-29 14:57:31 +0200

    actualizado

---
+++ actulziado/Equipo1-PagWeb-main/js/aficionados.js
@@ -0,0 +1,28 @@
+var nombre = prompt("Introduzca su nombre como aficionado "); //pedimos el nombre del aficionado
+var años =prompt("Introduzca los años que lleva como aficionado");//años que lleva el aficionado
+var equipo= "Equipo1"; //Nombre del equipo
+var dias = años*365;
+document.write("Hola <b>"+nombre + "</b> bienvenido a la web del equipo <b>" + equipo +"</b>.Gracias por acompañarnos durante estos <b>"+años+"</b> años, que son "+dias+" dias.");//mensaje una vez introduzcas los datos
+
+
+
+
+
+/* <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> */
+
+
+/*
+$(document).ready(function() {
+
+    var nombre = $("#name").val();
+    var año = $("#year").val();
+
+    $("#aceptar").click(function() {
+
+        alert(nombre);
+
+    });
+    
+
+
+});*/
